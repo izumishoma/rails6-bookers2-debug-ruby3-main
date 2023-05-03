dad3fd35c16864a3d81723b0138a2c6c44550e85
EC2 Default User <ec2-user@ip-172-31-22-182.ap-northeast-1.compute.internal>
2023-05-03 04:59:19 +0000

    7a

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,8 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+  has_many :favorited_users, through: :favorites, source: :user
+
   def self.looks(search, word)
     if search == "perfect_match"
       @book = Book.where("title LIKE?","#{word}")
@@ -16,10 +17,10 @@ class Book < ApplicationRecord
       @book = Book.all
     end
   end
-  
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
